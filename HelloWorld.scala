ae9135ee955a39ccf68f2d9cca85ad128f5cf245
Arvind RS <arvindrs.gb@gmail.com>
2017-05-08 21:38:02 -0400

    First scala app to print Hello World!

---
+++ HelloWorld.scala
@@ -0,0 +1,12 @@
+
+/*
+ * First example of Scala code to print the traditional "Hello, World!".
+ * Author: ArvindRS
+ * Date: 05/08/2017
+ */
+
+object HelloWorld {
+	def main(args: Array[String]) {
+		println("Hello, World!");
+	}
+}
